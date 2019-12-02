842a3b5c20b38ca61f52a29c90947c95dbd8b2c9
Mike Rossetti <s2t2@users.noreply.github.com>
2019-12-01 20:20:17 -0500

    Parse pertinent parts of the tweet

--- app/tweet_collector.py
+++ app/tweet_collector.py
@@ -25,23 +25,61 @@ class TweetCollector(StreamListener):
         self.auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
         self.api = tweepy.API(self.auth)
         self.counter = 0
-        self.max = 25 # TODO: set via env var
+        self.max = 25 # TODO: config via env var
+
+    def parse_status(self, status):
+        twt = status._json
+        usr = twt["user"]
+
+        if twt["truncated"] and twt["extended_tweet"]:
+            full_text = twt["extended_tweet"]["full_text"]
+        else:
+            full_text = twt["text"]
+
+        tweet = {
+            "id_str": twt["id_str"],
+            #"in_reply_to_status_id_str": twt["in_reply_to_status_id_str"],
+            "created_at": twt["created_at"], #> 'Mon Dec 02 01:06:52 +0000 2019'
+            #"timestamp_ms": twt["timestamp_ms"],
+            "geo": twt["geo"], #> None or __________
+            "full_text": full_text, #> 'Refuse censure! Make them try to impeach and beat it. Mr President you are guilty of no crime. Continue the exposure of these subversives that are so desperate to smear you for draining the swamp!'
+        }
+
+        user = {
+            "id_str": usr["id_str"],
+            "description": usr["description"],
+            "screen_name": usr["screen_name"],
+            "utc_offset": usr["utc_offset"],
+            "location": usr["location"],
+            "verified": usr["verified"],
+            #"geo_enabled": usr["geo_enabled"],
+        }
+
+        return tweet, user
+
+    def is_collectable(self, status):
+        if status.lang == "en" and status.user.verified:
+            return True
+        else:
+            return False
+
+    def on_status(self, status):
+        if self.is_collectable(status):
+            self.counter +=1
+            #if self.counter > self.max:
+            #    print("COLLECTION COMPLETE!")
+            #    print("SHUTTING DOWN...")
+            #    # TODO: exit() or return False or something
+            print("----------------")
+            print(f"DETECTED AN INCOMING TWEET! ({self.counter})")
+            #print(status.user.screen_name, "says:", status.text)
+            tweet, user = self.parse_status(status)
+            print("TWEET", tweet)
+            print("USER", user)
+            # todo: store tweet and user (to CSV in dev, to BQ in production)
 
     def on_connect(self):
         print("LISTENER IS CONNECTED!")
-
-    def on_status(self, status):
-        self.counter +=1
-        if self.counter > self.max:
-            print("COLLECTION COMPLETE!")
-            print("SHUTTING DOWN...")
-
-        print("----------------")
-        print(f"DETECTED AN INCOMING TWEET! ({self.counter})")
-        print(status.user.screen_name, "says:", status.text)
-        # todo: store the tweet! (to CSV in dev, to BQ in production)
-        breakpoint()
-        #pprint(status)
 
     def on_exception(self, exception):
         print("EXCEPTION:", type(exception))
