79bc0faa76f67a8c9c1349e2db9939ab3f3acdce
Mike Rossetti <s2t2@users.noreply.github.com>
2019-12-03 16:46:31 -0500

    Code review

--- test/storage_service_test.py
+++ test/storage_service_test.py
@@ -3,13 +3,13 @@ import pandas
 
 from app.storage_service import append_to_csv
 
-TEST_DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
-TEST_TWEETS_FILEPATH = os.path.join(TEST_DATA_DIR, "tweets.csv")
+DATA_DIR = os.path.join(os.path.dirname(__file__), "data")
+TWEETS_CSV_FILEPATH = os.path.join(DATA_DIR, "tweets.csv")
 
 def test_csv_collection():
 
-    if os.path.isfile(TEST_TWEETS_FILEPATH):
-        os.remove(TEST_TWEETS_FILEPATH)
+    if os.path.isfile(TWEETS_CSV_FILEPATH):
+        os.remove(TWEETS_CSV_FILEPATH)
 
     tweet = {
         'id_str': '1201316965991799999',
@@ -25,12 +25,12 @@ def test_csv_collection():
     }
 
     # if the local files don't already exist:
-    append_to_csv(tweet, tweets_filepath=TEST_TWEETS_FILEPATH)
-    assert os.path.isfile(TEST_TWEETS_FILEPATH)
-    tweets_df = pandas.read_csv(TEST_TWEETS_FILEPATH)
+    append_to_csv(tweet, tweets_filepath=TWEETS_CSV_FILEPATH)
+    assert os.path.isfile(TWEETS_CSV_FILEPATH)
+    tweets_df = pandas.read_csv(TWEETS_CSV_FILEPATH)
     assert len(tweets_df) == 1
 
     # if the local files already exist:
-    append_to_csv(tweet, tweets_filepath=TEST_TWEETS_FILEPATH)
-    tweets_df = pandas.read_csv(TEST_TWEETS_FILEPATH)
+    append_to_csv(tweet, tweets_filepath=TWEETS_CSV_FILEPATH)
+    tweets_df = pandas.read_csv(TWEETS_CSV_FILEPATH)
     assert len(tweets_df) == 2
