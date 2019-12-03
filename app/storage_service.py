3226a334622fbe065b9c8aaee7f2325b04d254a0
Mike Rossetti <s2t2@users.noreply.github.com>
2019-12-03 15:44:55 -0500

    Fix dup column

--- app/storage_service.py
+++ app/storage_service.py
@@ -13,23 +13,17 @@ DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
 TWEETS_CSV_FILEPATH = os.path.join(DATA_DIR, "tweets.csv")
 
 COLUMNS = [
-    "id_str", "user_id_str", "full_text", "geo", "created_at", #, "timestamp_ms" # TWEET
+    "id_str", "full_text", "geo", "created_at", #, "timestamp_ms" # TWEET
     "user_id_str", "user_screen_name", "user_description", "user_location", "user_verified" # USER
 ]
 
 def append_to_csv(tweet, tweets_filepath=TWEETS_CSV_FILEPATH):
+    # consider validating the tweet is a dict with an attribute for each column
     new_df = pandas.DataFrame([tweet], columns=COLUMNS)
 
     if os.path.isfile(tweets_filepath):
-        #df = pandas.read_csv(tweets_filepath)
-        #df.append(tweet, ignore_index=True)
-        #df.to_csv(tweets_filepath, mode="a", header=False, index=False) # this was exponentially growing the dataset filled with the first row only
-
         new_df.to_csv(tweets_filepath, mode="a", header=False, index=False)
     else:
-        #df = pandas.DataFrame([tweet], columns=COLUMNS)
-        #df.to_csv(tweets_filepath, index=False)
-
         new_df.to_csv(tweets_filepath, index=False)
 
 def append_to_bq(tweet, user):
